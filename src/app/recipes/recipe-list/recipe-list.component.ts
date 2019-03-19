4657cc440508b9cb2fa589817d7824e564ae6cdd
sgliput <sgliput@comcast.net>
2019-03-18 23:33:47 -0400

    added more features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipte= [];
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test.", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Cote_de_boeuf_p1040932.jpg/1200px-Cote_de_boeuf_p1040932.jpg")];
 
   constructor() { }
 
