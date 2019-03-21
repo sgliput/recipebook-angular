38b8fcde63746d968e9d3665ed3292e22f98e665
sgliput <sgliput@comcast.net>
2019-03-20 23:24:44 -0400

    working directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,18 +1,15 @@
-import { Directive, TemplateRef, ViewContainerRef, Input, HostListener, HostBinding } from "@angular/core";
-
+import { Directive, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
   selector: "[appDropdown]"
 })
 export class DropdownDirective{
 
-
   @HostBinding("class.open") isOpen = false;
   @HostListener("click") toggleOpen(event: Event){
     this.isOpen = !this.isOpen;
   }
 
-  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef){
-
+  constructor(){
   }
 }
